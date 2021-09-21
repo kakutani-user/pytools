dd1abf6192a67436e41033e6ea7922058f1b5f90
kakutani-user <dealus0609@gmail.com>
2021-09-21 18:59:25 +0900

    add real time plotter

--- serial_task/rt_plotter.py
+++ serial_task/rt_plotter.py
@@ -6,28 +6,76 @@ import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 import numpy as np
 import serial
+from serial.tools import list_ports
 import sys
+import datetime
+import csv
+
+DATA_NUM = 4
+SAMPLE_NUM = 10
+YRANGE_MIN = 0
+YRANGE_MAX = 100
+
+exec_time = datetime.datetime.now()
+
+def select_port():
+    ser = serial.Serial()
+    ser.baudrate = 115200
+    ser.timeout = 1
+
+    ports = list_ports.comports()
+    devices = [info.device for info in ports]
+
+    if len(devices) == 0:
+        print("error: device not found")
+        return None
+    elif len(devices) == 1:
+        print("only found %s" % devices[0])
+        ser.port = devices[0]
+    else:
+        # ポートが複数見つかった場合それらを表示し選択させる
+        for i in range(len(devices)):
+            print("input %3d: open %s" % (i, devices[i]))
+        print("input number of target port >> ",end="")
+        num = int(input())
+        ser.port = devices[num]
+
+    # 開いてみる
+    try:
+        ser.open()
+        return ser
+    except:
+        print("error when opening serial %s" % ser.port)
+        return None
 
 
 class PlotWindow:
     def __init__(self):
+        self.axi_num = DATA_NUM  # データ数
+        self.colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255), (255, 255, 255)]
         # プロット初期設定
         self.win = pg.GraphicsWindow()
         self.win.setWindowTitle(u"リアルタイムプロット")
         self.plt = self.win.addPlot()   # プロットのビジュアル関係
-        self.plt.setYRange(-5000000, 5000000)    # y軸の上限、下限の設定
-        self.curve = self.plt.plot()    # プロットデータを入れる場所
-        self.sample_num = 2048
+        self.plt.setYRange(YRANGE_MIN, YRANGE_MAX)    # y軸の上限、下限の設定
+        self.curve = list()
+        for i in range(self.axi_num):
+            self.curve.append(self.plt.plot(pen=self.colors[i]))    # プロットデータを入れる場所
+        # self.curve2 = self.plt.plot(pen=(0,255,0))    # プロットデータを入れる場所
+        self.sample_num = SAMPLE_NUM
 
-        self.ser = serial.Serial(port='/dev/cu.usbmodem2123201', baudrate=115200, timeout=1)
+        # self.ser = serial.Serial(port='/dev/cu.usbmodem2123201', baudrate=115200, timeout=1)
+        self.ser = select_port()
 
         # アップデート時間設定
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update)
-        self.timer.start(10)    # 10msごとにupdateを呼び出し
+        self.timer.start(0.1)    # 10msごとにupdateを呼び出し
 
         # UART受信データ(data)
-        self.data = [0]
+        self.data = list()
+        for i in range(self.axi_num):
+            self.data.append([0])
 
     def update(self):            
         # データの読み取り
@@ -35,21 +83,36 @@ class PlotWindow:
         if recv:
             # print(recv)
             str_value = recv.decode('utf-8')
-            value = str_value.strip()            # 文字列に含まれる空白を削除
+            # print(str_value)
+            rstr = str_value.strip()            # 文字列に含まれる空白を削除
+            values = rstr.split(',')
             # print(value)
-            sensor_value = int(value)
-
+            sensor_value = [float(s) for s in values]
+            print(sensor_value)
+            with open('data/sht31/{}.csv'.format(exec_time.strftime('%Y%m%d%H%M%S')), 'a') as f:
+                dt_now = datetime.datetime.now()
+                writer = csv.writer(f)
+                writer.writerow([dt_now] + sensor_value)
             # # 16進数2桁のデータ2つを一気にデコード
             # ReceData = np.frombuffer(ReceData,dtype=np.uint8)
 
-            self.data.append(sensor_value)
-            if len(self.data) > self.sample_num:
-                del self.data[0]
+            for i, val in enumerate(sensor_value):
+                if i > self.axi_num - 1:
+                    break
+                self.data[i].append(val)
+
+            now_data_len = len(self.data[0])
+            if now_data_len > self.sample_num * 10:
+                for d in self.data:
+                    del d[:self.sample_num * 5]
+                # del self.data[0]
 
             # lmax = max(self.data)
             # lmin = min(self.data)
             # self.plt.setYRange(lmin, lmax)
-            self.curve.setData(self.data)   # プロットデータを格納
+            self.plt.setXRange(now_data_len - self.sample_num, now_data_len)
+            for i, cur in enumerate(self.curve):
+                cur.setData(self.data[i])
             # self.ser.write(bytearray([70])) #デバック用送信器
 
 
@@ -57,58 +120,3 @@ if __name__ == "__main__":
     plotwin = PlotWindow()
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
-
-# # https://matplotlib.org/gallery/user_interfaces/embedding_in_tk_sgskip.html
-# # https://matplotlib.org/gallery/animation/simple_anim.html
-
-
-# import tkinter
-
-# from matplotlib.backends.backend_tkagg import (
-#     FigureCanvasTkAgg, NavigationToolbar2Tk)
-# from matplotlib.figure import Figure
-# import matplotlib.animation as animation
-
-# import numpy as np
-
-# import serial
-
-
-# def _quit():
-#     root.quit()     # stops mainloop
-#     root.destroy()  # this is necessary on Windows to prevent
-#                     # Fatal Python Error: PyEval_RestoreThread: NULL tstate
-
-
-# def init():  # only required for blitting to give a clean slate.
-#     line.set_ydata(np.sin(x))
-#     return line,
-
-
-# def animate(i):
-#     line.set_ydata(np.sin(x + i))  # update the data.
-#     return line,
-
-
-# root = tkinter.Tk()
-# root.wm_title("Embedding in Tk anim")
-
-# fig = Figure()
-# # FuncAnimationより前に呼ぶ必要がある
-# canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
-
-# x = np.arange(0, 3, 0.01)  # x軸(固定の値)
-# l = np.arange(0, 8, 0.01)  # 表示期間(FuncAnimationで指定する関数の引数になる)
-# plt = fig.add_subplot(111)
-# plt.set_ylim([-1.1, 1.1])
-# line, = plt.plot(x, np.sin(x))
-
-# ani = animation.FuncAnimation(fig, animate, l, init_func=init, interval=10, blit=True,)
-
-# toolbar = NavigationToolbar2Tk(canvas, root)
-# canvas.get_tk_widget().pack()
-
-# button = tkinter.Button(master=root, text="Quit", command=_quit)
-# button.pack()
-
-# tkinter.mainloop()
